6af93dd196ab2b1868095c5cffd5ff92f58eb07b
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-03-10 17:47:24 +0100

    Add posting, liking, retweeting, and commenting functionalities

---
+++ models/Tweet.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const tweetSchema = new mongoose.Schema({
+  content: { type: String, required: true, maxlength: 280 },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  createdAt: { type: Date, default: Date.now },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  retweets: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  comments: [
+    {
+      content: String,
+      author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      createdAt: { type: Date, default: Date.now },
+    },
+  ],
+});
+
+module.exports = mongoose.model("Tweet", tweetSchema);
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const User = require('./models/User'); 
-const authRoutes = require('./routes/authRoutes'); 
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const authRoutes = require("./routes/authRoutes");
+const tweetRoutes = require("./routes/tweetRoutes");
 
 const app = express();
 
-app.use(express.json()); 
+app.use(express.json());
 
-app.use(authRoutes);
+app.use("api/auth", authRoutes);
+app.use("/api/tweets", tweetRoutes);
 
-mongoose.connect(`${process.env.MONGO_URI}/twitter-clone`)
-    .then(() => {
-        console.log('Connected to the DB');
-        app.listen(3000, () => console.log('Server running on port 3000'));
-    })
-    .catch((error) => console.error(error));
+mongoose
+  .connect(`${process.env.MONGO_URI}/twitter-clone`)
+  .then(() => {
+    console.log("Connected to the DB");
+    app.listen(3000, () => console.log("Server running on port 3000"));
+  })
+  .catch((error) => console.error(error));
