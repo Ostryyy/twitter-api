1a318072e0bfa59f6a62e5db4aefa00fe7c3d22c
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-03-15 22:01:17 +0100

    Now we can set the port via .env file

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const tweetRoutes = require("./routes/tweetRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 app.use(express.json());
 
@@ -17,6 +22,6 @@ mongoose
   .connect(`${process.env.MONGO_URI}/twitter-clone`)
   .then(() => {
     console.log("Connected to the DB");
-    app.listen(3000, () => console.log("Server running on port 3000"));
+    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
   })
   .catch((error) => console.error(error));
